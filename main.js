6fc2ea4361277b8992f2965c6dc219bd1233bdf1
Wandering Crow <FraternaliAlessandro@gmail.com>
2020-03-26 13:45:22 +0100

    Sostituzione di 3 e multipli con Fizz completata

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 // CONSEGNA: creare un console.log che stampi i numeri da 1 a 100, e che sostituisca 3 e i suoi multipli con Fizz, 5 e i suoi multipli con Buzz e 15 e i suoi multipli con FizzBuzz
 
-// Stampa dei numeri da 1 a 100
+// Ciclo che gestisce i vari passaggi
 for (var i = 1; i <= 100; i++)  {
-  console.log(i);
+  // Stampa di Fizz al posto di 3 e i suoi multipli
+  if (i % 3 === 0) {
+    console.log("Fizz");
+  }
+  // Stampa dei numeri da 1 a 100
+  else {
+    console.log(i);
+  }
 }
-// Stampa di Fizz al posto di 3 e i suoi multipli
 
 // Stampa di Buzz al posto di 5 e i suoi multipli
 
