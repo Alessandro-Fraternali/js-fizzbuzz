360c3cf062954a7137bcaf783cb739be25a10544
Wandering Crow <FraternaliAlessandro@gmail.com>
2020-03-26 13:32:11 +0100

    Inserito ciclo for che stampa numeri da 1 a 100

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 // CONSEGNA: creare un console.log che stampi i numeri da 1 a 100, e che sostituisca 3 e i suoi multipli con Fizz, 5 e i suoi multipli con Buzz e 15 e i suoi multipli con FizzBuzz
 
 // Stampa dei numeri da 1 a 100
-
+for (var i = 1; i <= 100; i++)  {
+  console.log(i);
+}
 // Stampa di Fizz al posto di 3 e i suoi multipli
 
 // Stampa di Buzz al posto di 5 e i suoi multipli
