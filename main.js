c1b90edab37a2a9c19db8f710dda9c55f65b54bf
Wandering Crow <FraternaliAlessandro@gmail.com>
2020-03-26 13:26:55 +0100

    Inseriti i commenti guida

---
+++ main.js
@@ -0,0 +1,9 @@
+// CONSEGNA: creare un console.log che stampi i numeri da 1 a 100, e che sostituisca 3 e i suoi multipli con Fizz, 5 e i suoi multipli con Buzz e 15 e i suoi multipli con FizzBuzz
+
+// Stampa dei numeri da 1 a 100
+
+// Stampa di Fizz al posto di 3 e i suoi multipli
+
+// Stampa di Buzz al posto di 5 e i suoi multipli
+
+// Stampa di FizzBuzz al posto di 15 e i suoi multipli
